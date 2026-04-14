e21c8972cdbcc55fa492e4970168414fb77ab997
Alfonso <al570757@ucf.edu>
2026-04-14 15:37:53 -0400

    Add sound effect functions for game events

--- src/audio/sfx.c
+++ src/audio/sfx.c
@@ -1,1 +1,9 @@
 
+#include "sfx.h"
+#include "../drivers/buzzer.h"
+
+void SFX_Move(void)       { Buzzer_BeepMs(40); }
+void SFX_Error(void)      { Buzzer_BeepMs(200); }
+void SFX_Win(void)        { Buzzer_BeepMs(120); Buzzer_BeepMs(120); }
+void SFX_Tie(void)        { Buzzer_BeepMs(80); }
+void SFX_SwitchGame(void) { Buzzer_BeepMs(60); }
