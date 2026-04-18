29414e6ddb71c3222acb18a30e8645db0cd9f876
Alfonso <al570757@ucf.edu>
2026-04-18 02:02:22 -0400

    Update main.c

--- Tic-Tac-Toe/main.c
+++ Tic-Tac-Toe/main.c
@@ -1,28 +1,27 @@
-#include "msp430fr6989.h"
-#include "Grlib/grlib/grlib.h"
-#include "LcdDriver/lcd_driver.h"
-#include "display.h"
-#include "joystick.h"
 #include "sound.h"
 #include "hardware.h"
 #include "game.h"
 #include "menu.h"
 #include <stdlib.h>
 
-// Shared with game.c via extern
 extern volatile int activeBoard;
 extern volatile int needsRedraw;
 extern volatile int exitToMenu;
 
-// S1 toggles active board in dual mode
-// S2 returns to main menu
+// tracks whether we are in dual game mode
+volatile int dualMode = 0;
+
 #pragma vector = PORT1_VECTOR
 __interrupt void Button_ISR(void)
 {
     if(P1IFG & BIT1)
     {
-        activeBoard = (activeBoard == 1) ? 2 : 1;
-        needsRedraw = 1;
+        if(dualMode)
+        {
+            // Only switch boards in dual mode
+            activeBoard = (activeBoard == 1) ? 2 : 1;
+            needsRedraw = 1;
+        }
         P1IFG &= ~BIT1;
     }
     if(P1IFG & BIT2)
@@ -44,7 +43,6 @@ void main(void)
 
     srand(TA0R);
 
-    // Configure S1 (P1.1) and S2 (P1.2) with interrupts
     P1DIR &= ~(BIT1 | BIT2);
     P1REN |=  (BIT1 | BIT2);
     P1OUT |=  (BIT1 | BIT2);
@@ -57,7 +55,7 @@ void main(void)
     showWelcomeScreen(&ctx);
     buzzer_welcome();
 
-    // Wait for S1 to continue
+    // Wait for S1 to continue past welcome screen
     while((P1IN & BIT1) != 0);
     while((P1IN & BIT1) == 0);
     __delay_cycles(200000);
@@ -66,12 +64,33 @@ void main(void)
 
     while(1)
     {
+        dualMode = 0;   // reset before menu
         choice = run_main_menu(&ctx);
 
-        if(choice == OPT_PVP)        run_pvp(&ctx);
-        else if(choice == OPT_PVC)   run_pvc(&ctx);
-        else if(choice == DUAL_PVP)  run_dual_pvp(&ctx);
-        else if(choice == DUAL_PVC)  run_dual_pvc(&ctx);
-        else if(choice == DUAL_MIXED) run_dual_mixed(&ctx);
+        if(choice == OPT_PVP)
+        {
+            dualMode = 0;
+            run_pvp(&ctx);
+        }
+        else if(choice == OPT_PVC)
+        {
+            dualMode = 0;
+            run_pvc(&ctx);
+        }
+        else if(choice == DUAL_PVP)
+        {
+            dualMode = 1;   // enable S1 switching
+            run_dual_pvp(&ctx);
+        }
+        else if(choice == DUAL_PVC)
+        {
+            dualMode = 1;
+            run_dual_pvc(&ctx);
+        }
+        else if(choice == DUAL_MIXED)
+        {
+            dualMode = 1;
+            run_dual_mixed(&ctx);
+        }
     }
 }
