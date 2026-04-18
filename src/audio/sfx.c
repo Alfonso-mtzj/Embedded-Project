30717483387df11e0011ad66d8f05e4b805566db
Alfonso <al570757@ucf.edu>
2026-04-17 23:45:32 -0400

    Delete src directory

--- src/audio/sfx.c
+++
@@ -1,9 +0,0 @@
-
-#include "sfx.h"
-#include "../drivers/buzzer.h"
-
-void SFX_Move(void)       { Buzzer_BeepMs(40); }
-void SFX_Error(void)      { Buzzer_BeepMs(200); }
-void SFX_Win(void)        { Buzzer_BeepMs(120); Buzzer_BeepMs(120); }
-void SFX_Tie(void)        { Buzzer_BeepMs(80); }
-void SFX_SwitchGame(void) { Buzzer_BeepMs(60); }
