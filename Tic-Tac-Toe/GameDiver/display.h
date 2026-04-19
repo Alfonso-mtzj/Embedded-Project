a54c0a19b5232821ad956025b361550a7eedb660
Alfonso <al570757@ucf.edu>
2026-04-19 04:10:47 -0400

    Update display.h

--- Tic-Tac-Toe/GameDiver/display.h
+++ Tic-Tac-Toe/GameDiver/display.h
@@ -1,24 +1,27 @@
+// display.h
+// Declares all the functions used to draw on the LCD screen.
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
 #include "Grlib/grlib/grlib.h"
 #include "LcdDriver/lcd_driver.h"
 
-// System setup
+// clock and display setup
 void Initialize_Clock_System(void);
 void display_init(Graphics_Context *ctx);
 
-// Screens
+// welcome and end screens
 void showWelcomeScreen(Graphics_Context *ctx);
 void showWinner(Graphics_Context *ctx, char winner);
 void showTie(Graphics_Context *ctx);
 void showLoser(Graphics_Context *ctx);
 
-// Menu screens
+// menu screens
 void Menu(Graphics_Context *ctx, int selection);
 void Mult_game(Graphics_Context *ctx, int selection);
 
-// Board drawing
+// board drawing
 void drawBoard(Graphics_Context *ctx);
 void clearBoard(char grid[3][3]);
 void drawMarks(Graphics_Context *ctx, char grid[3][3]);
@@ -26,12 +29,10 @@ void drawX(Graphics_Context *ctx, int r, int c);
 void drawO(Graphics_Context *ctx, int r, int c);
 void drawCursor(Graphics_Context *ctx, int r, int c);
 void clearCell(Graphics_Context *ctx, int r, int c);
+void redrawCellLines(Graphics_Context *ctx, int r, int c);
 
-// Game info display
+// in-game info
 void showTurn(Graphics_Context *ctx, char player);
 void showGameNumber(Graphics_Context *ctx, int num);
 
-void redrawCellLines(Graphics_Context *ctx, int r, int c);
-
 #endif
-
