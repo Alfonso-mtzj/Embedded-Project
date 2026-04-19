7beee4bf905166e265a0e6f996c40d859e7153d9
Alfonso <al570757@ucf.edu>
2026-04-19 04:11:28 -0400

    Update joystick.h

--- Tic-Tac-Toe/GameDiver/joystick.h
+++ Tic-Tac-Toe/GameDiver/joystick.h
@@ -1,16 +1,21 @@
+// joystick.h
+// Defines direction codes and declares all joystick functions.
+
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 
 #include "sound.h"
 #include "hardware.h"
 
+// return values from readJoystick()
 #define CENTER 0
 #define RIGHT  1
 #define LEFT   2
 #define UP     3
 #define DOWN   4
-#define SELECT 5
+#define SELECT 5  // joystick button pushed in
 
+// ADC results shared across files
 extern int joyX;
 extern int joyY;
 
